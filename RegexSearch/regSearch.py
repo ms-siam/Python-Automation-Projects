7bbad56a1f47d8da4503d0cfbeff0cadb940eee6
ms-siam <mobaroksiam10@gmail.com>
2025-02-01 14:34:12 +0600

    Add initial implementation of regSearch.py for regex file searching

---
+++ RegexSearch/regSearch.py
@@ -0,0 +1,34 @@
+#! python3
+# regSearch.py - A program that opens all .txt files in a folder 
+#                and searches for any line that matches a user-supplied 
+#                regular expression. The results are then printed to the screen.
+
+
+from pathlib import Path
+import re
+
+#Asking user to input the regex to search for
+
+
+#Asking the user to input the directory path where .txt files are stored
+
+#Get all .txt file in the specified dir
+
+
+#For each .txt file in the folder
+
+    #Open the file for reading
+    
+    #Read the file line by line
+    
+    #For each line in the file
+    
+        #Search for the supplied regex in the line
+        
+        #If the regex is found'
+        
+            #Print the line that contains the regex
+            
+            #Print the file name
+            
+#close each file after processing
