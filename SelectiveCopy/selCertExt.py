dd0e4438f787eae8e755f6ae1bb7a37cd05b964d
ms-siam <mobaroksiam10@gmail.com>
2025-02-10 15:13:51 +0600

    Importing necessary modules

--- SelectiveCopy/selCertExt.py
+++ SelectiveCopy/selCertExt.py
@@ -2,6 +2,8 @@
 # selCertExt.py- walks through a folder tree, then searches for a certain
 # file extension like '.py', '.jpg' etc and copy them to a new folder 
 
+import shutil, re, os
+
 # Walking through the folder tree
 
 
