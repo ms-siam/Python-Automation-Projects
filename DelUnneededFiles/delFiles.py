884a47e7cd931fc0594fb0174205d11a0811bba0
ms-siam <mobaroksiam10@gmail.com>
2025-02-11 01:55:37 +0600

    saving the file path in a variable instead of appending it

--- DelUnneededFiles/delFiles.py
+++ DelUnneededFiles/delFiles.py
@@ -16,7 +16,7 @@ def delunneededfiles(source):
     for foldername, subsolders, filenames in os.walk(source):
         for file in filenames:
             file_size = os.path.getsize(os.path.join(foldername, file))
-            files.append(os.path.join(foldername, file))
+            file_path = os.path.join(foldername, file)
             fileno = fileno + 1
             if int(size[fileno]) > largesize:
                 largefiles.append(files[fileno])
