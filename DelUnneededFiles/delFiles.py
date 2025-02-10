10e05adb2f0711cd6d719b5896fa4eb5c302c87b
ms-siam <mobaroksiam10@gmail.com>
2025-02-11 01:25:31 +0600

    defined the large file size

--- DelUnneededFiles/delFiles.py
+++ DelUnneededFiles/delFiles.py
@@ -8,11 +8,12 @@ from pathlib import Path
 
 source = Path('F:\\STUDY')
 size = []
-
+largesize = 524,288,000
 def delunneededfiles(source, size):
     
     #Walking through the source folder tree
     for foldername, subsolders, filenames in os.walk(source):
         for file in filenames:
             size.append(os.path.getsize(file))
+        
 
