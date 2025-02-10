59bc3b0291f03babaff0442a8c4698ed59560e51
ms-siam <mobaroksiam10@gmail.com>
2025-02-10 15:13:19 +0600

    Skeleton of the main codes

--- SelectiveCopy/selCertExt.py
+++ SelectiveCopy/selCertExt.py
@@ -1,3 +1,11 @@
 #! python3
 # selCertExt.py- walks through a folder tree, then searches for a certain
 # file extension like '.py', '.jpg' etc and copy them to a new folder 
+
+# Walking through the folder tree
+
+
+# Searching for a certain file extension
+
+
+# Copy them to a new folder
