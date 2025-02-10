b72c6c9c56ef55a05826ddce8df5c0645be1e44c
ms-siam <mobaroksiam10@gmail.com>
2025-02-11 01:27:30 +0600

    creeated a variable named fileno to track the files

--- DelUnneededFiles/delFiles.py
+++ DelUnneededFiles/delFiles.py
@@ -9,11 +9,12 @@ from pathlib import Path
 source = Path('F:\\STUDY')
 size = []
 largesize = 524,288,000
+fileno = 0
 def delunneededfiles(source, size):
     
     #Walking through the source folder tree
     for foldername, subsolders, filenames in os.walk(source):
         for file in filenames:
             size.append(os.path.getsize(file))
-        
+    
 
