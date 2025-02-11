dda85647c408865bcc41c28e3c65bd9c31daae93
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 00:40:14 +0600

    imported pyperclip

--- ProjectmapItpyWithThewebbrowserModule/mapIt.py
+++ ProjectmapItpyWithThewebbrowserModule/mapIt.py
@@ -2,7 +2,7 @@
 # mapIt.py- Launches a map in the browser using an address 
 # from the command line or clipboard
 
-import webbrowser, sys
+import webbrowser, sys, pyperclip
 if len(sys.argv) > 1:
     #Get address from command line
     address = ' '.join(sys.argv[1:])
