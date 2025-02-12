8d4128e919fe20cdaf4c17ad52f21a7dd6e50a60
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 22:17:31 +0600

    Added direction for further codings

--- ProjectDownloadAllXKCDComics/downloadXkcd.py
+++ ProjectDownloadAllXKCDComics/downloadXkcd.py
@@ -6,4 +6,13 @@ import requests, os, bs4
 url = 'https://xkcd.com'
 os.makedirs('xkcd', exist_ok=True)
 while not url.endswith('#'):
+    #Download the page
+    
+    #Find the url of the comic image
+    
+    #Download the image
+    
+    #Save the image to ./xkcd
+    
+    #Get the Prev button's url
     
