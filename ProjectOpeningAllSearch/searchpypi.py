4b578f81efadf043914fb43baa2f94bc339e9c27
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 19:26:36 +0600

    Add comments for further coding direction

--- ProjectOpeningAllSearch/searchpypi.py
+++ ProjectOpeningAllSearch/searchpypi.py
@@ -6,3 +6,7 @@ import requests, sys, webbrowser, bs4 # type: ignore
 print('Searching...')      # Display text while downloading the search result
 res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
 res.raise_for_status()
+
+#Todo: Retrieve the top search results
+
+#Todo: Open a browser tab for each results
