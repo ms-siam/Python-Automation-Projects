be0152662d3dcbb4f2cac2a1b37dc53e73f095c9
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 19:20:57 +0600

    Display text while downloading the search result

--- ProjectOpeningAllSearch/searchpypi.py
+++ ProjectOpeningAllSearch/searchpypi.py
@@ -2,3 +2,5 @@
 # searchpypi.py- Opens several search results.
 
 import requests, sys, webbrowser, bs4 # type: ignore
+
+print('Searching...')      # Display text while downloading the search result
