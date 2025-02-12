e2cb11c345c1e29d37073104c01150a833bc8f5e
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 22:51:38 +0600

    Added for loop to write out chuks of the image data

--- ProjectDownloadAllXKCDComics/downloadXkcd.py
+++ ProjectDownloadAllXKCDComics/downloadXkcd.py
@@ -26,6 +26,9 @@ while not url.endswith('#'):
     
         #Save the image to ./xkcd
         imageFile = open(os.path.join('xkcd', os.path.basename(comicUrl)), 'wb')
+        for chunk in res.iter_content(100000):
+            imageFile.write(chunk)
+        imageFile.close()
     
     #Get the Prev button's url
     
