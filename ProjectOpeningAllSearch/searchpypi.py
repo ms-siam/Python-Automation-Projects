92f7a0f00971e05c6bdcd2a9f4f5090fcdf791e5
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 19:25:38 +0600

    Being sure if the pages downloaded successfully

--- ProjectOpeningAllSearch/searchpypi.py
+++ ProjectOpeningAllSearch/searchpypi.py
@@ -5,3 +5,4 @@ import requests, sys, webbrowser, bs4 # type: ignore
 
 print('Searching...')      # Display text while downloading the search result
 res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
+res.raise_for_status()
