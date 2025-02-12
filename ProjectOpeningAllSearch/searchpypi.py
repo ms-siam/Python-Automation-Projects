0138a52c09d89be9c1e007e08dd753a8de184a15
ms-siam <mobaroksiam10@gmail.com>
2025-02-12 21:32:41 +0600

    Corrected the version of python idle

--- ProjectOpeningAllSearch/searchpypi.py
+++ ProjectOpeningAllSearch/searchpypi.py
@@ -1,7 +1,7 @@
 #! python3
 # searchpypi.py- Opens several search results.
 
-import requests, sys, webbrowser, bs4 # type: ignore
+import requests, sys, webbrowser, bs4 
 
 print('Searching...')      # Display text while downloading the search result
 res = requests.get('https://google.com/search?q=' 'https://pypi.org/search/?q=' + ' '.join(sys.argv[1:]))
