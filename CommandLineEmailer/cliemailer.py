f4c1fbc86c9805bf58692ee6618493a6d39e6b80
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 15:55:03 +0600

    Replace webdriver.Chrome() with undetected_chromedriver.Chrome()

--- CommandLineEmailer/cliemailer.py
+++ CommandLineEmailer/cliemailer.py
@@ -1,8 +1,8 @@
 #! python3
 # cliEmailer.py- takes an email address and string of text , 
 # then login to email account and sends the email to the provided address
-
-from selenium import webdriver
+import undetected_chromedriver as uc
+# from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.by import By
