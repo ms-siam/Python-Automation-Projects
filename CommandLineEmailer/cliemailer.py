5d86edd494acdd2af902b6d0406581b341053da1
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 13:25:41 +0600

    imported webdriver

--- CommandLineEmailer/cliemailer.py
+++ CommandLineEmailer/cliemailer.py
@@ -2,4 +2,4 @@
 # cliEmailer.py- takes an email address and string of text , 
 # then login to email account and sends the email to the provided address
 
-import selenium
+from selenium import webdriver
