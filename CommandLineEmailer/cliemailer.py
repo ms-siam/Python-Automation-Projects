bc9b2dc304a82adfb62ac29e53e7247a35836ffe
ms-siam <mobaroksiam10@gmail.com>
2025-02-13 13:58:59 +0600

    imported EC from selenium

--- CommandLineEmailer/cliemailer.py
+++ CommandLineEmailer/cliemailer.py
@@ -4,6 +4,7 @@
 
 from selenium import webdriver
 from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
 
 browser = webdriver.Firefox()
 browser.get('https://accounts.google.com/')
